8085f353405553811931615ed9d9c5779f46e9c8
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-06-22 19:54:35 +0200

    finished adding the appility of modifying the task name

--- lib/components/TaskList/TaskList.dart
+++ lib/components/TaskList/TaskList.dart
@@ -10,30 +10,32 @@ class TaskList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final tasks = Provider.of<Tasks>(context).tasks(parent: parent);
-    print('[Adham][$parent] $tasks');
+    //print('[Adham][$parent] $tasks');
     return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: tasks.isEmpty
-          ? Center(child: Column(
-            children: [
-              Icon(Icons.running_with_errors_sharp,size: 130,color:Colors.blue[800]),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('No Tasks',style: TextStyle(fontSize: 130/4,color: Colors.blueAccent[700])),
+      child: Container(
+        child: tasks.isEmpty
+            ? Center(child: Column(
+              children: [
+                Icon(Icons.running_with_errors_sharp,size: 130,color:Colors.blue[800]),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('No Tasks',style: TextStyle(fontSize: 130/4,color: Colors.blueAccent[700])),
+                ),
+              ],
+              mainAxisSize: MainAxisSize.min,
+            ))
+            : ListView.builder(
+                itemBuilder: (ctx, index) {
+                  return TaskListItem(
+                    targetTask: tasks[index],
+                    key: ObjectKey(tasks[index]),
+                  );
+                },
+                itemCount: tasks.length,
+                shrinkWrap: true,
               ),
-            ],
-            mainAxisSize: MainAxisSize.min,
-          ))
-          : ListView.builder(
-              itemBuilder: (ctx, index) {
-                return TaskListItem(
-                  targetTask: tasks[index],
-                  key: ObjectKey(tasks[index]),
-                );
-              },
-              itemCount: tasks.length,
-              shrinkWrap: true,
-            ),
+      ),
     );
   }
 }
--- lib/screens/TaskScreen.dart
+++ lib/screens/TaskScreen.dart
@@ -5,19 +5,51 @@ import 'package:todo/components/AddingTaskFloatingActionButton.dart';
 import 'package:todo/components/TaskList/TaskList.dart';
 import 'package:todo/providers/Tasks.dart';
 
-class TaskScreen extends StatelessWidget {
+class TaskScreen extends StatefulWidget {
   const TaskScreen({Key? key}) : super(key: key);
   static const String routeName = "/TaskScreen";
 
   @override
+  _TaskScreenState createState() => _TaskScreenState();
+}
+
+class _TaskScreenState extends State<TaskScreen> {
+  @override
   Widget build(BuildContext context) {
     final task = ModalRoute.of(context)!.settings.arguments as Task;
+    final TextEditingController _controller =
+        new TextEditingController(text: task.title);
+    print(task);
     return Scaffold(
       appBar: AppBar(
         title: Text(task.title),
       ),
-      body: TaskList(parent: task.id,),
-      floatingActionButton: AddingTaskFloatingActionButton(parent: task.id,),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _controller,
+              onSubmitted: (value) {
+                setState(() {
+                  Provider.of<Tasks>(context, listen: false)
+                      .changeTitleOfTask(taskName: value, id: task.id!);
+                });
+              },
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15))),hintText: "Your Task Title..."),
+            ),
+          ),
+          Expanded(
+              child: TaskList(
+            parent: task.id,
+          )),
+        ],
+      ),
+      floatingActionButton: AddingTaskFloatingActionButton(
+        parent: task.id,
+      ),
     );
   }
 }
